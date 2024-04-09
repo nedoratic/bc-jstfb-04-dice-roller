53e75150cbc4483c844cd8a961c6b5fed1bca319
nedoratic <nedoratic@gmail.com>
2024-04-09 18:40:58 +0200

    index.js - display variable added

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const btnAddDice = document.querySelector('.btn--add-dice');
 const btnReset = document.querySelector('.btn--reset');
 const btnRollDice = document.querySelector('.btn--roll-dice');
 const btnRemoveDice = document.querySelector('.btn--remove-dice');
+
+// Display
+const display = document.querySelector('.app__display');
