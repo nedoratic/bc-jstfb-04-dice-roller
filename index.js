d1024e9bdbcab1693a35f8e9cdce404a9c7fe6db
nedoratic <nedoratic@gmail.com>
2024-04-09 18:38:51 +0200

    index.js - buttons variables added

---
+++ index.js
@@ -0,0 +1,5 @@
+// Buttons
+const btnAddDice = document.querySelector('.btn--add-dice');
+const btnReset = document.querySelector('.btn--reset');
+const btnRollDice = document.querySelector('.btn--roll-dice');
+const btnRemoveDice = document.querySelector('.btn--remove-dice');
