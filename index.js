e85d8ad6cdbe95ea87d5293fc83c3a19e8197271
nedoratic <nedoratic@gmail.com>
2024-04-10 20:40:37 +0200

    index.js - add dice function added

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 // Buttons
 const btnAddDice = document.querySelector('.btn--add-dice');
+const btnRemoveDice = document.querySelector('.btn--remove-dice');
 const btnReset = document.querySelector('.btn--reset');
 const btnRollDice = document.querySelector('.btn--roll-dice');
-const btnRemoveDice = document.querySelector('.btn--remove-dice');
 
 // Display
 const display = document.querySelector('.app__display');
 
 // Get random number from 1 to 6
 const getRandomNumber = () => Math.floor(Math.random() * 6) + 1;
+
+// Add Dice
+const addDice = () => {
+	const dice = document.createElement('div');
+	dice.classList.add('dice');
+	dice.innerHTML = rollDice();
+	display.appendChild(dice);
+};
